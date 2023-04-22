fda16dfcf6e097af1f8501247f1e5cfb0477a543
GusDamPer <gusdamper@gmail.com>
2023-04-22 11:46:31 -0300

    Implementacion de funcion buscar al id n° 1

--- src/main/java/com/porfolio/GustavoP/Controller/PersonaController.java
+++ src/main/java/com/porfolio/GustavoP/Controller/PersonaController.java
@@ -53,4 +53,10 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         return persona;
     }
+    
+    @GetMapping("/personas/ver/perfil")
+    public Persona findPersona() {
+        return ipersonaService.findPersona((long)1);
+    }
+    
 }
