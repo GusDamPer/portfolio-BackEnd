6e1ddd3d2dbb1332c4241c77c17c343513084e51
GusDamPer <gusdamper@gmail.com>
2023-04-25 15:23:35 -0300

    actualizacion desde notebook, Login con JWT

--- src/main/java/com/porfolio/GustavoP/Security/MainSecurity.java
+++ src/main/java/com/porfolio/GustavoP/Security/MainSecurity.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -44,6 +45,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
                 .anyRequest().authenticated()
                 .and().exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
@@ -51,7 +53,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
         return super.authenticationManager();
     }
 
-    @Override
+    @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
