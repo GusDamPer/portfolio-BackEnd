847ea8ccaee7fecd79a44c1e52d1904c7cf23768
GusDamPer <gusdamper@gmail.com>
2023-04-24 23:50:46 -0300

    actualizacion desde notebook, Login con JWT

--- src/main/java/com/porfolio/GustavoP/Security/Service/UsuarioService.java
+++ src/main/java/com/porfolio/GustavoP/Security/Service/UsuarioService.java
@@ -14,7 +14,7 @@ public class UsuarioService {
     @Autowired
     iUsuarioRepository iusuarioRepository;
     
-    public Optional<Usuario> getByNombreusuario(String nombreUsuario){
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
         return iusuarioRepository.findByNombreUsuario(nombreUsuario);
     }
     
