0c97a444d30de87b18a06d5404fa50b01a2b2e0d
GusDamPer <gusdamper@gmail.com>
2023-04-28 14:23:48 -0300

    se implementa Educacion al back

---
+++ src/main/java/com/porfolio/GustavoP/Dto/dtoEducacion.java
@@ -0,0 +1,41 @@
+
+package com.porfolio.GustavoP.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+
+public class dtoEducacion {
+    @NotBlank
+    private String nombreE;
+    @NotBlank
+    private String descripcionE;
+    
+    //constructor
+
+    public dtoEducacion() {
+    }
+
+    public dtoEducacion(String nombreE, String descripcionE) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+    
+    //getters & setters
+
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescripcionE() {
+        return descripcionE;
+    }
+
+    public void setDescripcionE(String descripcionE) {
+        this.descripcionE = descripcionE;
+    }
+    
+}
---
+++ src/main/java/com/porfolio/GustavoP/Repository/REducacion.java
@@ -0,0 +1,13 @@
+
+package com.porfolio.GustavoP.Repository;
+
+import com.porfolio.GustavoP.Entity.Educacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface REducacion extends JpaRepository<Educacion, Integer>{
+    public Optional<Educacion> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
